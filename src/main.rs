e580ef3db4b5f3500f5689b2b1bb6b5310062d0c
Yuki Kodama <endflow.net@gmail.com>
2019-11-24 01:59:57 +0900

    Raytracing

--- src/main.rs
+++ src/main.rs
@@ -7,49 +7,56 @@ mod point;
 mod sphere;
 mod vector;
 
-use crate::point::Point3D;
-use crate::vector::{General, Vector3D};
+use crate::canvas::Canvas;
+use crate::coordinate as coord;
+use crate::point::{Point2D, Point3D};
+use crate::sphere::Sphere;
 use piston_window::*;
 
 fn main() {
-    let camera = Point3D {
-        x: 50.0,
-        y: 10.0,
-        z: 150.0,
-    };
+    let canvas = Canvas::new(50.0, 50.0, 75.0, 100.0, 100.0);
+    let camera = Point3D::new(50.0, 50.0, 150.0);
+    let obj1 = Sphere::new(50.0, 20.0, 50.0, 20.0);
 
-    let light = Point3D {
-        x: 50.0,
-        y: 100.0,
-        z: 75.0,
-    };
+    let width: usize = 100;
+    let height: usize = 100;
+    let mut buf = vec![];
 
-    let v1 = Vector3D::<General>::new(10.0, 20.0, 30.0);
-    println!("v1={:?}", v1);
-
-    let l1 = v1.norm();
-    println!("v1.len={}", l1);
-
-    let n1 = v1.normalize();
-    println!("n1={:?}", n1);
-
-    let l2 = n1.norm();
-    println!("n1.len={}", l2);
-
-    let mut window: PistonWindow = WindowSettings::new("rustracer", [100, 100])
+    let mut window: PistonWindow = WindowSettings::new("rustracer", [width as u32, height as u32])
         .exit_on_esc(true)
         .build()
         .unwrap();
 
+    let black = [0.0, 0.0, 0.0, 1.0];
+    let white = [1.0, 1.0, 1.0, 1.0];
+
+    println!("Rendering...");
+    for i in 0..10000 {
+        // Raytracing
+        // let i = buf.len();
+        let x = i % width;
+        let y = (i - x) / width;
+        let pos = Point2D::<coord::Screen>::screen_at(x as i32, y as i32);
+        let ray = canvas.cast_ray(&pos, &camera);
+
+        let pixel = match obj1.intersection(&ray) {
+            Some(_) => white,
+            None => black,
+        };
+        buf.push(pixel);
+    }
+    println!("Done");
+
     while let Some(event) = window.next() {
         window.draw_2d(&event, |ctx, g, _device| {
-            clear([1.0; 4], g);
-            rectangle(
-                [0.0, 0.0, 1.0, 1.0],
-                [25.0, 25.0, 1.0, 1.0],
-                ctx.transform,
-                g,
-            );
+            // Rendering
+            for x in 0..width {
+                for y in 0..height {
+                    let i = y * width + x;
+                    let pixel = if i < buf.len() { buf[i] } else { black };
+                    rectangle(pixel, [x as f64, y as f64, 1.0, 1.0], ctx.transform, g);
+                }
+            }
         });
     }
 }
